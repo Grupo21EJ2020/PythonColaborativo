d9c110a8af4f090a7274a36f13dc707b347465ae
Juan Gabriel Lopez Solorzano <juangabriel.lopezsr@gmail.com>
2020-05-06 14:23:47 -0500

    Menu creado

---
+++ main.py
@@ -0,0 +1,7 @@
+print("Bienvenido a mi programa")
+print("1.-Pasatiempos de LiiGabriel")
+opcion = int(input("Elige una opcion"))
+
+if opcion == 1:
+    print("Eligio los pasatiempos del usuario 1")
+
