d57b3760cabe43772fff1ee5db4dd2fbda9d6a1f
denilson <islasdeni@gmail.com>
2020-05-06 16:02:37 -0500

    Correcion de usuarios y eliminacion de lineas

--- main.py
+++ main.py
@@ -3,15 +3,13 @@ print("1.-Pasatiempos de LiiGabriel")
 print("2.-Pasatiempos de TheOmarNajera")
 print("3.-Pasatiempos de DenilsonIslas")
 print("4.-Pasatiempos otro alumno")
-<<<<<<< HEAD
-print("20.-Pasatiempos de SammyDeAztlan")
-=======
 print("5.-Pasatiempos de DanielCruz2906")
 print("6.-Pasatiempo de Emmanuelk3")
 print("7.- Pasatiempo de MarianoAbimael")
 print("8.-Pasatiempo de pb1598")
 print("9.-Pasatiempo de Edgar Zamora")
->>>>>>> e60b43e2f07c8195a97823b7fde178c651830dcf
+print("10.-Pasatiempos de SammyDeAztlan")
+
 
 opcion = int(input("Elige una opcion"))
 
@@ -20,18 +18,9 @@ if opcion == 1:
 elif opcion == 2:
     print("Eligio los pasatiempos del usuario 2")
 elif opcion == 3:
-        print("Eligio los pasatiempos del usuario 3")
+    print("Eligio los pasatiempos del usuario 3")
 elif opcion == 4:
-        print("Eligio los pasatiempos del otro alumno")
-<<<<<<< HEAD
-
-elif opcion ==3:
-    print ("Eligio los pasatiempos del usuario 3")
-elif opcion ==4:
-    print ("Eligio los pasatiempos del usuario 4")
-elif opcion ==20:
-    print("Eligio los pasatiempos del usuario 20")
-=======
+    print("Eligio los pasatiempos del otro alumno")
 elif opcion ==5:
     print("Eligio los pasatiempos del usuario 5")
 elif opcion ==6:
@@ -42,4 +31,6 @@ elif opcion ==8:
     print ("Eligio los pasatiempos del usuario 8")  
 elif opcion ==9:
     print ("Eligio los pasatiempos del usuario 9")  
->>>>>>> e60b43e2f07c8195a97823b7fde178c651830dcf
+elif opcion ==10:
+    print ("Eligio los pasatiempos del usuario 10")  
+
