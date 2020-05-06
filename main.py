deb6805650e21ecc52f15251e18f0b9f45c13da0
denilson <islasdeni@gmail.com>
2020-05-06 14:37:11 -0500

    Add opcion 3

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 print("Bienvenido a mi programa")
 print("1.-Pasatiempos de LiiGabriel")
 print("2.-Pasatiempos de TheOmarNajera")
+print("3.-Pasatiempos de DenilsonIslas")
 opcion = int(input("Elige una opcion"))
 
 if opcion == 1:
     print("Eligio los pasatiempos del usuario 1")
 elif opcion == 2:
     print("Eligio los pasatiempos del usuario 2")
+elif opcion == 3:
+        print("Eligio los pasatiempos del usuario 3")
 
