850e66d065f7fbfd96a444e068c533fcaee81462
DanielCruz2906 <danielcruz75357@gmail.com>
2020-05-06 14:56:17 -0500

    se agrego otro usuario

--- main.py
+++ main.py
@@ -1,30 +1,23 @@
 print("Bienvenido a mi programa")
 print("1.-Pasatiempos de LiiGabriel")
-<<<<<<< HEAD
-print("2.-Pasatiempos de DanielCruz2906")
-=======
 print("2.-Pasatiempos de TheOmarNajera")
 print("3.-Pasatiempos de DenilsonIslas")
 print("4.-Pasatiempos otro alumno")
+print("5.-Pasatiempos de DanielCruz2906")
 
->>>>>>> 51f81cf7046e2ef9a52a68aa6316dc52264ffb71
 opcion = int(input("Elige una opcion"))
 
 if opcion == 1:
     print("Eligio los pasatiempos del usuario 1")
-<<<<<<< HEAD
-if opcion == 2:
-    print("Eligio los pasatiempos del usuario 2")
-=======
 elif opcion == 2:
     print("Eligio los pasatiempos del usuario 2")
 elif opcion == 3:
         print("Eligio los pasatiempos del usuario 3")
 elif opcion == 4:
         print("Eligio los pasatiempos del otro alumno")
->>>>>>> 51f81cf7046e2ef9a52a68aa6316dc52264ffb71
-
 elif opcion ==3:
     print ("Eligio los pasatiempos del usuario 3")
 elif opcion ==4:
     print ("Eligio los pasatiempos del usuario 4")
+elif opcion ==4:
+    print ("Eligio los pasatiempos del usuario 5")    
