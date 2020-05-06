5543dbcaa1a4c42109fbbbf65b0630679864758a
Omar Najera <omar.navzz@gmail.com>
2020-05-06 14:32:45 -0500

    Agregué usuario 2 y opción 2

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 print("Bienvenido a mi programa")
 print("1.-Pasatiempos de LiiGabriel")
+print("2.-Pasatiempos de TheOmarNajera")
 opcion = int(input("Elige una opcion"))
 
 if opcion == 1:
     print("Eligio los pasatiempos del usuario 1")
+elif opcion == 2:
+    print("Eligio los pasatiempos del usuario 2")
 
