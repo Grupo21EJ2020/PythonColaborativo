bc433c6219bec7f438e857550a0674f7db9a3a9d
Juan Gabriel Lopez Solorzano <juangabriel.lopezsr@gmail.com>
2020-05-06 14:38:45 -0500

    agregue el codigo de otro alumno(lo ayude)

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 print("Bienvenido a mi programa")
 print("1.-Pasatiempos de LiiGabriel")
+print("2.-Pasatiempos otro alumno")
 opcion = int(input("Elige una opcion"))
 
 if opcion == 1:
     print("Eligio los pasatiempos del usuario 1")
-
+elif opcion == 2:
+    print("Eligio los pasatiempos del usuario 2")
