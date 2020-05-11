7f93ede04a14d0920c26598fb5abd0ad3f0330d1
Daniel Alejandro Siller Garcia <danny.999@live.com.mx>
2020-05-11 18:00:04 -0500

    Agregue mi archivo 29daniel29siller

---
+++ 29daniel29siller.py
@@ -0,0 +1,6 @@
+class 29Daniel29Siller ():
+    def imprimirPasatiempos(self):
+        print("Pasatiempos:")
+        print("Ver videos TOP 10 Peruanos Moment")
+        print("Jugar Minecraft")
+        
