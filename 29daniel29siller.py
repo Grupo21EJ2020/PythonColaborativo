08aa6c1b1c8f37cf838ae247f9fab5766adc6368
Daniel Alejandro Siller Garcia <danny.999@live.com.mx>
2020-05-11 18:15:03 -0500

    borre mi carpeta para hacer otra bien

--- 29daniel29siller.py
+++
@@ -1,6 +0,0 @@
-class 29Daniel29Siller ():
-    def imprimirPasatiempos(self):
-        print("Pasatiempos:")
-        print("Ver videos TOP 10 Peruanos Moment")
-        print("Jugar Minecraft")
-        
