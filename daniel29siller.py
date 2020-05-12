e378c53751c8d676d31faf6262101ad44c4b5fbc
Daniel Alejandro Siller Garcia <danny.999@live.com.mx>
2020-05-11 19:00:24 -0500

    Tuve que cambiar el nombre de la carpeta y la clase para poder guardarlo

---
+++ daniel29siller.py
@@ -0,0 +1,6 @@
+class Daniel_Siller ():
+    def imprimirPasatiempos(self):
+        print ("Pasatiempos")
+        print("Ver videos de Top Peruanos moment")
+        print("Dibujar")
+        print("Jugar Minecraft")
