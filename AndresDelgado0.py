ced0661bdd7387ea16ecf4f7010280de0fbe32f2
Andres Eduardo Delgado Galván <andreshog@gmail.com>
2020-05-12 15:27:33 -0500

    Se corrigieron algunos datos

---
+++ AndresDelgado0.py
@@ -0,0 +1,6 @@
+class Andres_Delgado0():
+    def imprimirPasatiempos(self):
+        print("Pasatiempos: ")
+        print("Jugar videojuegos")
+        print("Leer")
+        print("Ver peliculas") 
