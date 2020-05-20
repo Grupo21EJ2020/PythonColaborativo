c8d505683eb3ab337eeb56530ff3e3071094dfc0
SOFIA QUIROZ <SofiaMonserrat524@hotmail.com>
2020-05-20 15:01:31 -0500

    Agregue clases de sofia

---
+++ sofiamonserat524.py
@@ -0,0 +1,17 @@
+def imprimir (self):
+        print (f"Nombre de la marca:{self.marca} y el color es:{self.color}")
+
+class Papas ():
+    def __init (self,marca,sabor):
+        self.marca=marca
+        self.sabor=sabor
+    def impimir (self):
+        print (f"Nombre de la marca{self.marca} y su sabor es:{self.sabor}")
+
+class Gorras():
+    def __init (self,colore):
+        self.color=color
+    def imprimir (self) :
+         print (f"El color de la gorra es:{self.color)")
+        
+
