0ff731efd7c19304692c3fa34c18be08a63caccd
EEAM-ElPolloSad <61396750+EEAM-ElPolloSad@users.noreply.github.com>
2020-05-20 16:56:06 -0500

    Hice mi clase

---
+++ my.py
@@ -0,0 +1,9 @@
+Clase  EduardoArriaga :
+     def  imprimirPasatiempos ( self ):
+             print ( "Pasatiempos:" )
+             print ( "Leer" )
+             print ( "Jugar videojuegos" )
+             print ( "Cocinar" )
+             print ( "Jugar en la pc" )
+             print ( "Limpiar" )
+             print ( "Ver peliculas" )
