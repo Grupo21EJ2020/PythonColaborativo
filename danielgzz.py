a8cf3702d3f85fcf7eecf294ad8bbaca6465875a
Daniel Gonzalez Capetillo <daniecgzz@gmail.com>
2020-05-21 02:46:34 -0500

    Nueva clase

---
+++ danielgzz.py
@@ -0,0 +1,7 @@
+class danielgzz:
+      def imprimirPasaDaniel(self):
+            imprimir("Pasatiempos:")
+            imprimir("Escuchar Musica")
+            imprimir("Ver series")
+            imprimir("Ver Peliculas")
+            
