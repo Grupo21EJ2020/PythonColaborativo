352c62707e8981ad4143ec340dcc0aa3e86bf522
EEAM-ElPolloSad <61396750+EEAM-ElPolloSad@users.noreply.github.com>
2020-05-21 13:36:59 -0500

    Add files via upload

---
+++ mi clase jeje.py
@@ -0,0 +1,8 @@
+clase  CarlosGuajardo ():
+     def  imprimirPasatiempos ( self ):
+             imprimir ( "Pasatiempos:" )
+             imprimir ( "Jugar loteria" )
+             imprimir ( "Salir con amigos" )
+             imprimir ( "Jugar en la PC" )
+             imprimir ( "Salir a pasear a mi perro" )
+             imprimir ( "Ver pelis" )
