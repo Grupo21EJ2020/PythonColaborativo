ef81cb68e1f09973a8dd201cd8a0791626137461
Aranza Jaqueline Gonzalez Castillo <aranza.gonzalezcl@uanl.edu.mx>
2020-05-22 03:37:07 -0500

    Restaurado

--- main.py
+++ main.py
@@ -1,6 +1,146 @@
-print("Bienvenido a mi programa")
-print("1.-Pasatiempos de Ara002")
-opcion = int(input("Elige una opcion"))
-if opcion == 1:
-    print("Eligio los pasatiempos del usuario 1")
-    
+from liigabriel import Liigabriel    
+from pb1598 import Pb1598
+from thomasmedina01 import ThomasMedina01
+from denilsonislas import DenilsonIslas
+from liigabriel import Liigabriel 
+from jairtg02 import JairTG02             
+from liigabriel import Liigabriel             
+from marianoabimael import Marianoabimael
+from TheOmarNajera import TheOmarNajera   
+from liigabriel import Liigabriel
+from marianoabimael import Marianoabimael
+from edithvidales import EdithVidales
+from israel391 import Israel391
+from antoniogzz90 import AntonioGzz90
+from danielcruz2906 import Danielcruz2906
+from alejandraa07 import AlejandraA07
+from daniel29siller import Daniel_Siller
+from ltiNacianceno import LTINacianceno
+from alejandroroch import AlejandroRCH
+from diegoalz import diegoalz
+from saraosorio import Saraosorio
+from AndresDelgado0 import Andres_Delgado0
+from kozel99 import Kozel99
+from sammydeaztlan import Sammydeaztlan      
+from Emmanuelk3 import Emmanuelk3
+from edgarzamoraa import Edgarzamoraa
+from luisajasso import LuisaJasso
+from paulinam01 import PaulinaM01
+from Alejandro01598 import Alejandro01598
+from danielgzz import danielgzz
+print("1.-Pasatiempos de LiiGabriel")
+print("2.-Pasatiempos de TheOmarNajera")
+print("3.-Pasatiempos de DenilsonIslas")
+print("4.-Pasatiempos otro alumno")
+print("5.-Pasatiempos de DanielCruz2906")
+print("6.-Pasatiempo de Emmanuelk3")
+print("7.-Pasatiempo de MarianoAbimael")
+print("8.-Pasatiempo de pb1598")
+print("9.-Pasatiempo de Edgar Zamora")
+print("10.-Pasatiempos de SammyDeAztlan")
+print("11.-Pasatiempos de Kozel99")
+print("12.-Pasatiempos de AlejandraA07")
+print("13.-Pasatiempos de JairTG02")
+print("14.-Pasatiempos de Israel391")
+print("15.-Pasatiempos de Juan Nacianceno21")
+print("16.-Pasatiempos de Otro Alumno")
+print("17.-Pasatiempos de diegoalz")
+print("18.-Pasatiempos de 29Daniel29Siller")
+print("19.-Pasatiempos de ThomasMedina01")
+print("20.-Pasatiempos de Andres-Delgado0")
+print("21.-Pasatiempos de Sara-Osorio")
+print("22.- Pasatiempos de Diego Antonio")
+print("23.- Pasatiempos de AlejandroRCH")
+print("24.- Pasatiempos de EdithVidales")
+print("25.- Pasatiempos de PaulinaM01")
+print("26.- Pasatiempos de Luisajasso")
+print("27.-Pasatiempos de Alejandro01598")
+print("28.- Pasatiempo de Daniel-Gzz")
+print("29.- Pasatiempo de danna-borda02")
+opcion = int(input("Elige una opcion: "))
+objetoPasatiempo = Liigabriel()
+    objetoPasatiempo.imprimirPasatiempos()
+elif opcion == 2:
+    objetoOmar = TheOmarNajera()
+    objetoOmar.imprimirPasOmar()
+elif opcion == 3:
+    objetoDenilson = DenilsonIslas()
+    objetoDenilson.imprimirPasatiempos()
+elif opcion == 4:
+    print("Eligio los pasatiempos del otro alumno")
+elif opcion ==5:
+    objetoPasatiempo = Danielcruz2906()
+    objetoPasatiempo.imprimirPasatiempos()
+elif opcion ==6:
+   objetoPasatiempo = Emmanuelk3()
+   objetoPasatiempo.imprimirPasatiempos()
+elif opcion ==7:   
+    objetoPasatiempo = Marianoabimael()
+    objetoPasatiempo.imprimirPasatiempos()
+elif opcion ==8:
+    objetoPasatiempo = Pb1598()
+    objetoPasatiempo.imprimirPasatiempos()
+elif opcion ==9:
+    objetoEZ = Edgarzamoraa()
+    objetoEZ.imprimirpasatiempos()
+elif opcion ==10:
+    objetoPasatiempo = Sammydeaztlan()
+    objetoPasatiempo.imprimirPasatiempos()
+elif opcion ==11:
+    objetoPasatiempo = Kozel99()
+    objetoPasatiempo.imprimirPasatiempos()
+elif opcion ==12:
+    objetoPasatiempo = AlejandraA07()
+    objetoPasatiempo.imprimirPasatiempos()
+elif opcion ==13:
+    objetoPasatiempo = JairTG02()
+    objetoPasatiempo.imprimirpasatiempos()
+elif opcion ==14:
+    objetoPasatiempo = Israel391()
+    objetoPasatiempo.Imprimirpasatiempos()
+elif opcion ==15:
+    objetoPasatiempo = LTINacianceno()
+    objetoPasatiempo.imprimirPasatiempos()
+elif opcion ==16:
+    print("Eligio los pasatiempos del usuario 16")
+elif opcion ==17:
+    objetoPasatiempo = diegoalz()
+    objetoPasatiempo.imprimirpasatiempos()
+elif opcion ==18:
+    objetoPasatiempo = Daniel_Siller()
+    objetoPasatiempo.Imprimirpasatiempos()
+elif opcion ==19:
+    objetoPasatiempo = ThomasMedina01()
+    objetoPasatiempo.imprimirPasatiempos()
+elif opcion ==20:
+    objetoPasatiempo = Andres_Delgado0()
+    objetoPasatiempo.imprimirPasatiempos()
+elif opcion ==21:
+     objetoPasatiempo = Saraosorio
+     objetoPasatiempo.imprimirPasatiempos()
+elif opcion ==22:
+    objetoPasatiempo = AntonioGzz90()
+    objetoPasatiempo.imprimirPasatiempos()
+elif opcion ==23:
+    objetoPasatiempo = AlejandroRCH()
+    objetoPasatiempo.imprimirPasatiempos()
+    print("Eligio los pasatiempos del usuario 23")
+elif opcion ==24:
+    objetoPasatiempo = EdithVidales()
+    objetoPasatiempo.imprimirPasatiempos() 
+elif opcion ==25:
+    objetoPasatiempo = PaulinaM01()
+    objetoPasatiempo.Imprimirpasatiempos()
+elif opcion ==26:
+    objetoPasatiempo = LuisaJasso()
+    objetoPasatiempo.Imprimirpasatiempos()   
+elif opcion == 27:
+    objetoAlejandro01598 = Alejandro01598()
+    objetoAlejandro01598.imprimirpasatiempos()
+elif opcion == 28:
+    objetodanielgzz= danielgzz()
+    objetodanielgzz.imprimirpasatiempos()
+elif opcion == 29:
+    objetoPasatiempo = danna-borda02()    
+    objetoPasatiempo.imprimirPasatiempos()
+    print("Eligio los pasatiempos del usuario 29") 
