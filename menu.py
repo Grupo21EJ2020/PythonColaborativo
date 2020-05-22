ccf1600b19075cee7bb1bce43792408e1b775e86
Aranza Jaqueline Gonzalez Castillo <aranza.gonzalezcl@uanl.edu.mx>
2020-05-21 23:14:39 -0500

    Menu creado

---
+++ menu.py
@@ -0,0 +1,6 @@
+print("Bienvenido a mi programa")
+print("1.-Pasatiempos de Ara002")
+opcion = int(input("Elige una opcion"))
+if opcion == 1:
+    print("Eligio los pasatiempos del usuario 1")
+    
