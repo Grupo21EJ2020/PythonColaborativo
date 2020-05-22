b0a3a9cb07601699813caab3851229f5c54f6426
Aranza Jaqueline Gonzalez Castillo <aranza.gonzalezcl@uanl.edu.mx>
2020-05-21 21:52:10 -0500

    Profe lo guarde con mi nombre y no como Main

---
+++ Ara002.py
@@ -0,0 +1,7 @@
+class Ara002():
+    def imprimirPasatiempos(self):
+        print("Pasatiemos: ")
+        print("Escuchar Musica")
+        print("Ver series")
+        print("Jugar juegos de mesa con mi mamá")
+        
