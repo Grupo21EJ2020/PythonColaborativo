e86993fe71c007e2e61b30b77309b0dbf28fa54c
EEAM-ElPolloSad <61396750+EEAM-ElPolloSad@users.noreply.github.com>
2020-05-22 19:23:41 -0500

    Delete my.py

--- my.py
+++
@@ -1,9 +0,0 @@
-Clase  EduardoArriaga :
-     def  imprimirPasatiempos ( self ):
-             print ( "Pasatiempos:" )
-             print ( "Leer" )
-             print ( "Jugar videojuegos" )
-             print ( "Cocinar" )
-             print ( "Jugar en la pc" )
-             print ( "Limpiar" )
-             print ( "Ver peliculas" )
