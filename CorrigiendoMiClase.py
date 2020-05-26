264035d9167c0ee32d67e582be8f0f72c73eba3d
MAXIMILIANO <maxalvarez18@hotmail.com>
2020-05-25 23:34:00 -0500

    Mi clase corregida

---
+++ CorrigiendoMiClase.py
@@ -0,0 +1,7 @@
+class Max_Alvarez()
+    def imprimirPasatiempos(self):
+        print("pasatiempos max")
+        print("Ver videos")
+        print("jugar con mi gato")
+        print("hablar con amigos")
+        print("buscar novia")
