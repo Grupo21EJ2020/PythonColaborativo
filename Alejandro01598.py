6ed1fa2e410ff2a285a4e1a37711e5ab566d957e
Alejandro01598 <alejajosez7@gmail.com>
2020-05-25 22:35:47 -0500

    Agregue mis pasatiempos

---
+++ Alejandro01598.py
@@ -0,0 +1,6 @@
+class Alejandro01598():
+    def imprimirpasatiempos(self):
+        print("Pasatiempos:")
+        print("ver peliculas")
+        print("jugar videojuegos")
+        print("estudiar")
