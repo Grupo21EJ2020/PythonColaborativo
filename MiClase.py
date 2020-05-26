674dc584b4f05a7dc7e5b9fc89cab8698572fa93
MAXIMILIANO <maxalvarez18@hotmail.com>
2020-05-25 23:29:55 -0500

    agregué mi clase Max

---
+++ MiClase.py
@@ -0,0 +1,8 @@
+class Max_Alvarez():
+    def ImprimirPasatiempos(self)
+    print("pasatiempos")
+    print("ver videos")
+    print("dibujar")
+    print("jugar con mi gato")
+    print("buscar novia")")
+    print("buscar novia")
