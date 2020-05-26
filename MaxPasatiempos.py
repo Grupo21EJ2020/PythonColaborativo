dc645a6912a9c6e132ce9e4bd337aa4f1469851f
MAXIMILIANO <maxalvarez18@hotmail.com>
2020-05-25 21:14:02 -0500

    Hice mis pasatiempos

---
+++ MaxPasatiempos.py
@@ -0,0 +1,18 @@
+print("Bienvenido a mis pasatiempos")
+print("")
+print("1.- Jugar xbox")
+print("2.- Hacer ejercicio")
+print("3.- Pasear a mi perro")
+print("4.- Salir")
+
+if opcion == 1:
+    print("Has entrado a jugar xbox")
+
+elif opcion == 2:
+    print("Has entrado a hacer ejercicio")
+
+elif opcion == 3:
+    print("Has entrado a paser a mi perro")
+
+elif opcion == 4:
+    print("Has salido del sistema!")
