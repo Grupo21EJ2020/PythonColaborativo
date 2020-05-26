2de31920c6c33dfb30a7aa0e1b121120028ce9ad
Daniel087-pixel <mpdaniel37@gmail.com>
2020-05-25 21:34:06 -0500

    Agregue mis clases

--- Daniel087-pixel.py
+++ Daniel087-pixel.py
@@ -1,1 +1,6 @@
-Daniel Mendoza
+class Daniel087-pixel ():
+    def imprimirPasatiempos(self):
+        print ("Pasatiempos")
+        print("Jugar Skullgirls")
+        print("Reparar cosas")
+        print("Leer Ciencia Ficcion")
