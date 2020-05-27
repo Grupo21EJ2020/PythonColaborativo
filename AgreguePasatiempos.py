402b606e31e6fe13994648f4157ad2c4c72f84e6
Aranza Jaqueline Gonzalez Castillo <aranza.gonzalezcl@uanl.edu.mx>
2020-05-26 20:13:59 -0500

    Agregue mis pasatiempos

---
+++ AgreguePasatiempos.py
@@ -0,0 +1,6 @@
+class Ara002():
+    def imprimirPasatiempos(self):
+        print("Pasatiemos: ")
+        print("Escuchar Musica")
+        print("Ver series")
+        print("Jugar juegos de mesa con mi mamá")
